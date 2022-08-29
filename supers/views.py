3f7108d5c7a8fbfbf0abc33c318d88f66f097ba6
Howard Gill <theogill1369@gmail.com>
2022-08-28 20:33:52 -0400

    Created POST functionality for super

--- supers/views.py
+++ supers/views.py
@@ -5,12 +5,20 @@ from .models import Super
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 
 def supers_list(request):
     
-    supers = Super.objects.all()
-
-    serializer = SuperSerializer(supers, many=True)
-    
-    return Response(serializer.data)
+    if request.method == 'GET':
+        supers = Super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+   
+   
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
